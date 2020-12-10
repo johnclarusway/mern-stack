2462ef0e497b33420c0c24d350badee3aa1f2e4b
John <john@clarusway.com>
2020-12-10 22:58:06 +0200

    Add auth register todo

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,5 +1,17 @@
 exports.authRegister = (req, res) => {
-  //TODO: Register func.
+  const { firstName, lastName, email, password } = req.body;
+  console.log(
+    "Fields:",
+    firstName,
+    lastName,
+    email,
+    password
+  );
+  // TODO1: Validate the fields
+  // TODO2: check already registered
+  // TODO3: crpyt password
+  // TODO4: save the user to DB
+
   res.send("Register Completed.");
 };
 
--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const connectDB = require("./models/connectDB");
 const router = require("./routes/router");
 
 connectDB();
+
+app.use(express.json());
 app.use("/api", router);
 
 app.listen(process.env.PORT, () => {
