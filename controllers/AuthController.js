c40a08b474d0ea4110c0db2a39776defa4e420c0
John <john@clarusway.com>
2020-12-12 16:43:22 +0200

    Add save user model

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,17 +1,23 @@
-exports.authRegister = (req, res) => {
+const User = require("../models/UserModel");
+
+exports.authRegister = async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
-  console.log(
-    "Fields:",
-    firstName,
-    lastName,
-    email,
-    password
-  );
+
   // TODO1: Validate the fields
   // TODO2: check already registered
   // TODO3: crpyt password
   // TODO4: save the user to DB
 
+  const user = new User({
+    firstName,
+    lastName,
+    email,
+    password, //crypted password
+  });
+
+  await user.save();
+
+  //TODO: Error handling for saving
   res.send("Register Completed.");
 };
 
