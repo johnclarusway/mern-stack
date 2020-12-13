f66eda49d3d499fc6757ac98bddc4f695632fae8
John <john@clarusway.com>
2020-12-13 18:32:42 +0200

    Add prodcution case

--- server.js
+++ server.js
@@ -11,6 +11,14 @@ connectDB();
 app.use(express.json());
 app.use("/api", router);
 
+// production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`I'm listening on port ${port}`);
